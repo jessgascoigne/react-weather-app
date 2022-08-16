c7cccdf6206133ed0b09dda810feee28f701bc55
jessgascoigne <jessagascoigne@gmail.com>
2022-08-16 18:19:20 -0500

    Fixed hour and AM/PM format

--- src/Header.js
+++ src/Header.js
@@ -31,6 +31,13 @@ export default function Header(props) {
   const month = months[current.getMonth()];
   const date = current.getDate();
   let hour = current.getHours();
+  let timestamp = hour >= 12 ? "PM" : "AM";
+  if (hour === 0) {
+    hour = 12;
+  } else if (hour >= 13) {
+    hour = hour - 12;
+  }
+
   let minutes = current.getMinutes();
   if (minutes <= 9) {
     minutes = `0${minutes}`;
@@ -40,7 +47,7 @@ export default function Header(props) {
     <div className="Header">
       <h1 className="city-name">{props.data}</h1>
       <h3 className="date-time">
-        {weekday} | {month} {date} | {hour}:{minutes} AM
+        {weekday} | {month} {date} | {hour}:{minutes} {timestamp}
       </h3>
     </div>
   );
