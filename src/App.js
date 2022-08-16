6153e4f422eaff289e9bda597453be190751d515
jessgascoigne <jessagascoigne@gmail.com>
2022-08-16 17:19:57 -0500

    Imported initial search form & bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from "react";
+import Search from "./Search";
+import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="body">
+        <Search defaultCity="Seattle" />
+      </div>
+    </div>
+  );
 }
